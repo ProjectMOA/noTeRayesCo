a50c2f261f281b1aed4233eaa540b178f97ae067
Nicu1309 <nicu1309@gmail.com>
2017-05-19 17:34:11 +0200

    Fix typo

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var app = express();
 app.use(express.static('public'));
 
 var connection = mysql.createConnection({
-  host     : process.env.MYSQL_HOST ||,
-  user     : process.env.MYSQL_USER ||,
-  password : process.env.MYSQL_PASS ||,
-  database : process.env.MYSQL_DB ||
+  host     : process.env.MYSQL_HOST,
+  user     : process.env.MYSQL_USER,
+  password : process.env.MYSQL_PASS,
+  database : process.env.MYSQL_DB
 });
 
 connection.connect(function(err) {
