93118dd493d5fea536accf61f00e1a271a3d7914
Inaki Abadia <i.abadiaosta@gmail.com>
2017-08-09 11:59:15 +0200

    Updates database connection: Specifies port aswell

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.static('public'));
 
 var connection = mysql.createConnection({
   host     : process.env.MYSQL_HOST,
+  port     : process.env.MYSQL_PORT,
   user     : process.env.MYSQL_USER,
   password : process.env.MYSQL_PASS,
   database : process.env.MYSQL_DB
