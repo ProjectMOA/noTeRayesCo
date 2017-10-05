317030e39790756959eebe042ae794a51ee2d979
Inaki Abadia <i.abadiaosta@gmail.com>
2017-10-05 15:34:26 +0100

    Values showing at once now

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -26,25 +26,37 @@ angular.module('noTeRayesCoApp', [])
         $scope.countDownDate = new Date("Oct 7, 2017 21:00:00").getTime();
 
         // Update the count down every 1 second
-        $scope.countDownFunction = $interval(function () {
 
-          // Get todays date and time
-          $scope.now = new Date().getTime();
+        var updateCounter = function () {
 
-          // Find the distance between now an the count down date
-          $scope.distance = $scope.countDownDate - $scope.now;
+            // Get todays date and time
+            $scope.now = new Date().getTime();
+            
+            // Find the distance between now an the count down date
+            $scope.distance = $scope.countDownDate - $scope.now;
+            
+            // Time calculations for days, hours, minutes and seconds
+            $scope.days = Math.floor($scope.distance / (1000 * 60 * 60 * 24));
+            $scope.hours = Math.floor(($scope.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            $scope.minutes = Math.floor(($scope.distance % (1000 * 60 * 60)) / (1000 * 60));
+            $scope.seconds = Math.floor(($scope.distance % (1000 * 60)) / 1000);
+            
+            if ($scope.distance <= 0) {
+                $scope.pilares = true;
+                // If the count down is over, the interval is stopped
+                $interval.cancel($scope.countDownFunction);
+            }
+        }
 
-          // Time calculations for days, hours, minutes and seconds
-          $scope.days = Math.floor($scope.distance / (1000 * 60 * 60 * 24));
-          $scope.hours = Math.floor(($scope.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          $scope.minutes = Math.floor(($scope.distance % (1000 * 60 * 60)) / (1000 * 60));
-          $scope.seconds = Math.floor(($scope.distance % (1000 * 60)) / 1000);
+        $scope.countDownFunction = $interval(updateCounter, 1000);
 
-          if ($scope.distance <= 0) {
-            $scope.pilares = true;
-            // If the count down is over, the interval is stopped
-            $interval.cancel($scope.countDownFunction);
-          }
-        }, 1000);
+        updateCounter();
+
+        // Once everything is loaded, make it all appear!
+        var hiddenElems = document.getElementsByName("init-hidden");
+        var elem;
+        for (elem = 0; elem < hiddenElems.length; elem++){
+            hiddenElems[elem].style.visibility = 'visible';
+        }
 
     }]);
